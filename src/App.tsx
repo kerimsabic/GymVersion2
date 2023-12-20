1aeadeb3b092208fb1b83b38b491baff1c31e13b
Kerim <kerim.sabic.207@gmail.com>
2023-12-20 20:39:08 +0100

    Fixed problems with unused imports

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,4 @@
 import { useEffect, useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 import Navbar from '@/scenes/NavBar'
 import Home from '@/scenes/Home'
