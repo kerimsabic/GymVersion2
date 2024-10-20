c2792b1266e1c1eff21784936eb38931745bcfa9
Kerim <kerim.sabic@klika.ba>
2024-10-20 15:39:31 +0200

    deploying

--- src/scenes/Register/Register.tsx
+++ src/scenes/Register/Register.tsx
@@ -249,7 +249,7 @@ const handleStripe = async (id : any, trainingPlanId : any, rawPrice:any, name:a
               </div>
               <div>
                 <label className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Username</label>
-                <input type="text" name="username" id="username" value={formData.username} onChange={handleInputChange} className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="123 Main St" />
+                <input type="text" name="username" id="username" value={formData.username} onChange={handleInputChange} className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Newusername" />
                 {errors.username && <p className="text-red-500 text-sm">{errors.username}</p>}
               </div>
               <div>
